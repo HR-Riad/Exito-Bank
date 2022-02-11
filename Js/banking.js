5a9f816288808e2b6de3e45e49bf6118b49ed765
Md. Hasanur Rahman Riad <57297996+HR-Riad@users.noreply.github.com>
2022-02-11 21:26:40 +0600

    Money withdraw and reduce balance for withdraw

--- Js/banking.js
+++ Js/banking.js
@@ -38,4 +38,40 @@ balanceTotal.innerText =   newBalanceTotal;
 //   clear the deposite input field
 depositInput.value = '';
 
+});
+
+/* handle withdraw event handler */
+document.getElementById('withdraw-button').addEventListener('click',function(){
+    const withdrawtInput = document.getElementById('withdraw-input');
+    const withdrawAmountText = withdrawtInput.value;
+
+    const newWithdrawAmount = parseFloat(withdrawAmountText);
+   
+
+    // set Withdraw Total
+
+    const withdrawTotal = document.getElementById('withdraw-total');
+    const previousWithdrawText = withdrawTotal.innerText;
+    const previousWithdrawTotal = parseFloat(previousWithdrawText);
+
+    const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+
+    withdrawTotal.innerText = newWithdrawTotal;
+
+
+    // update balance
+
+    const balanceTotal = document.getElementById('balance-total');
+
+     const previousBalanceText = balanceTotal.innerText;
+     const previousBalanceTotal = parseFloat(previousBalanceText);
+
+     const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+
+     balanceTotal.innerText = newBalanceTotal;
+
+    // clear withdraw input
+
+    withdrawtInput.value = '';
+
 })
